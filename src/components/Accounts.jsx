e551804d6a4aae150ed2840b233aae28d09bfbae
luizajuridicomgmultas-design <luizajuridicomgmultas@gmail.com>
2025-11-01 21:44:26 -0300

    Update Accounts.jsx

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -1,6 +1,6 @@
 import { Button } from './ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
-import Input from './ui/input';
+import { Label } from './ui/label';
 import { Label } from './ui/label';
 import Select, { SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
 import { useToast } from './ui/use-toast';
